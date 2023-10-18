85d9b665dcf1b0071695003f37f09700b8c83970
Lllewisreynolds <lsreynolds108@gmail.com>
2023-10-18 16:03:50 +0100

    Erroneous if statement removed from initial window prompt as this should immediately appear / prior to any user input. Was affecting flow of overall generatePassword() function code block. Flow checked through console logs. All previously global scope variants moved to their respective sub-functions within generatePassword() as local scope. Used .confirm() method  to confirm the user has used the set criteria for lowercase password characters. Will duplicate next for other character types

--- Develop/script.js
+++ Develop/script.js
@@ -1,60 +1,81 @@
-// An array has been created and populated for each Character Set the user will choose their password from.
 
 // Do these need to be global, or can all of the code I create go within the generatePassword() function?
-
-var lowerCaseCharacter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCaseCharacter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numberCharacter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharacter = ["@", "!",  "#", "$", "%", "&", "*", "(", ")", "^", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", "<", ">", "=", "_", "`", "|", "~", ".",];
-
-// Blank array created. This will be used to append the user's choices from each Character Set defined above. Use push?
-
-var selectedPasswordArray = [];
-
-var userSelection;
+// Confused on global and local scope with the userSelection variable and the functions enclosed within the code block for generatePassword() also? Will the later run 
 
 
-// Use .confirm() method to confirm password is acceptable to user "Do you want this?" - provides a boolean - once password is input
+
 
 
 
   function generatePassword() {
-    promptAndCheckLength()
-    // promptChar()
-    // checkChar()
-    // createPassword()
+    console.log("generating my password running")
+    var passwordLength = promptAndCheckLength()
+    console.log("Password Length is: ", passwordLength);
+    var selectedPasswordArray = promptAndCheckChar()
+    console.log("My selected password array is: ", selectedPasswordArray)
+  var userPassword = ""
+  for (let i = 0; i < passwordLength; i++) {
+    var newLetter; // = //get random element from selectedPasswordArray
+    //add newLetter to userPassword
+    
   }
-  
+    console.log("generating password done.")
+  return userPassword
+  }
+  //console.log(generatePassword());
+ 
   function promptAndCheckLength() {
+    console.log("About to get length")
     // Prompt created to elicit initial user response
-    if (!userSelection) {
-      var userSelection = window.prompt("What length password would you like?");
-    }
+    var userSelection = window.prompt("What length password would you like?");
+
     // Alert created if password length does not meet set criteria
     if ((userSelection < 8) || (userSelection > 128)) {
       window.alert("Password must be a length of at least 8 characters and no more than 128 characters");
       window.location.reload(true)
     }
     // Reset created if initial user response is not a number
+    // Why does Not a Number work but Number.isNan not work if it is more specific?
     if (isNaN(userSelection)) {
-      window.alert("Password must be in numerical format");
+      window.alert("Password length must be in numerical format");
       userSelection = 0;
       window.location.reload(true)
     }
+    console.log("Done prompting for length")
     return userSelection
   }
-    
-    // ## Acceptance Criteria
-  
-  
-  // THEN I am presented with a series of prompts for password criteria
-  // Use .prompt() function to prompt the user to follow the set criteria for password characters
-  // Use .confirm() function to confirm the user has used the set criteria for password characters
-  
+
+    // THEN I am presented with a series of prompts for password criteria
+  // Use .confirm() to confirm the user has used the set criteria for password characters
+  // Use .confirm() method to confirm password is acceptable to user "Do you want this?" - provides a boolean
+
+
+function promptAndCheckChar() {
+  // An array has been created and populated for each Character Set the user will choose their password from.
+  var lowerCaseCharacter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCaseCharacter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numberCharacter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialCharacter = ["@", "!",  "#", "$", "%", "&", "*", "(", ")", "^", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", "<", ">", "=", "_", "`", "|", "~", ".",];
+  // Blank array created. This will be used to append the user's choices from each Character Set defined above.
+  var userSelection = [];
+  var lowerCaseConfirm = window.confirm('Would you like to include lowercase characters?');
+  if (lowerCaseConfirm === true) {
+      userSelection.push(lowerCaseCharacter);
+  }
+  if(userSelection.length === 0 ){
+    console.log("INVALID")
+    //troubleshoot here
+  }
+  return userSelection;
+}  
   // WHEN asked for character types to include in the password
   // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
   // Use .confirm() function
-  
+
+
+
+    
+    // ## Acceptance Criteria
    
   // WHEN I answer each prompt
   // THEN my input should be validated and at least one character type should be selected
@@ -83,3 +104,7 @@ function writePassword() {
 
 generateBtn.addEventListener("click", writePassword);
 
+// example for loop - will be used as template for final password generation + then randomisation
+// for (let i = 0; i < 10; i++) {
+//   console.log(i)
+// }
